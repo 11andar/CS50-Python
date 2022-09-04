54b79fd51424a8e6f586af0fdd3b93c8ee22eaf3
Jedrek Darowski <11andar07@gmail.com>
2022-09-04 14:36:26 +0200

    1. TODO list

---
+++ deep.py
@@ -0,0 +1,10 @@
+def main():
+    # get input from user
+
+    # implement 'check' function
+
+    # output "yes" if the answear is "42" "forty-two" or "forty two"
+
+    # else output "no"
+
+main()
