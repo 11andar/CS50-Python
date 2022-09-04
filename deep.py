f0750f673a72a5fcbaf8ade411f0b950f9575651
Jedrek Darowski <11andar07@gmail.com>
2022-09-04 14:43:25 +0200

    3.Implemented 'check' function,  added 'lower' fuction to answear, created a list which contains right answears

--- deep.py
+++ deep.py
@@ -1,12 +1,20 @@
 def main():
     # get input from user
-    answear = input("What's the answear? ")
-    check()
+    answear = input("What's the answear? ").lower()
+    check(answear)
 
-    # implement 'check' function
-
+# implement 'check' function
+def check(a):
+    a_list = ["42", "forty-two", "forty two"]
+    
     # output "yes" if the answear is "42" "forty-two" or "forty two"
-
+    if a == a_list[0]:
+        print("Yes")
+    elif a == a_list[1]:
+        print("Yes")
+    elif a == a_list[2]:
+        print("Yes")
+    
     # else output "no"
 
 main()
