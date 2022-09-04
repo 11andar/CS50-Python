ac082ac3b558cd3cb4f593e3d3410b4ddbd21906
Jedrek Darowski <11andar07@gmail.com>
2022-09-04 18:04:05 +0200

    1.TODO list

---
+++ files.py
@@ -0,0 +1,12 @@
+def main():
+# prompt user for file name and extention
+    
+# implement function that checks the file extention
+def extention():
+# create list of available files
+
+# recognize file by ".file_extention"
+
+# else output aplication/octet-stream
+
+main() 
