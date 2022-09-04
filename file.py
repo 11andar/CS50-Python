50cccad53dc0744f3be5e3890406e0ef6bb8f083
Jedrek Darowski <11andar07@gmail.com>
2022-09-04 18:25:52 +0200

    1. TODO list

---
+++ file.py
@@ -0,0 +1,14 @@
+def main():
+#prompt user for file name and extension
+
+
+def extention():
+
+#create list of available file extensions
+
+#recognize file by ".extension"
+
+#else output application/octet-stream
+
+
+main()
