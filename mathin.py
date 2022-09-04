5b23e53e349f97ac6e61c6a534f97e7dbdfab534
Jedrek Darowski <11andar07@gmail.com>
2022-09-04 19:04:22 +0200

    1. TODO list

---
+++ mathin.py
@@ -0,0 +1,12 @@
+def main():
+# prompt user for an arithmetic expression
+
+# assign values to variables x, y, z
+
+# change x and z into floats
+
+# create lists of operators and mathematical operations
+
+# output appropriate operations and their results
+
+main() 
