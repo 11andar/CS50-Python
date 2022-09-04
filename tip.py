9fabdd49d220400922bcf7b88878ab149c7e7a66
Jedrek Darowski <11andar07@gmail.com>
2022-09-04 13:33:16 +0200

    3. Calculate tip

--- tip.py
+++ tip.py
@@ -3,8 +3,10 @@ def main():
     # Get input from user (meal prize and tip percentage)
     dollars = dollars_to_float(input("Prize: "))
     percent = percent_to_float(input("Percentage of the tip: "))
-    
+
     # Calculate tip
+    tip = dollars * percent
+    
     # Output tip value
 
 def dollars_to_float(d):
