1815200526de7ea695261ae612ad278772e662c7
Jedrek Darowski <11andar07@gmail.com>
2022-09-04 18:09:18 +0200

    3. Created list of files

--- files.py
+++ files.py
@@ -7,9 +7,11 @@ def main():
 # implement function that checks the file extention
 def extention():
 # create list of available files
+    file_extentions = ["GIF/.gif", "IMAGE/.jpeg", "PNG/.png", "PDF/.pdf", "TXT/.txt", "ZIP/.zip"]
+    error = ["application/octet-stream"]
 
 # recognize file by ".file_extention"
 
-# else output aplication/octet-stream
+# else output application/octet-stream
 
 main() 
