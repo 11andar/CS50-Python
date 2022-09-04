bb8d1b37ea0787d9aae869e6d98cfa361cce46fc
Jedrek Darowski <11andar07@gmail.com>
2022-09-04 15:32:09 +0200

    1. TODO list

---
+++ bank.py
@@ -0,0 +1,12 @@
+def main():
+# prompt user for greeting
+
+# implement function which checks greeting
+
+# output $0 if the greeting contains "hello"
+
+# output $20 if the greeting startswith "h" but not "hello"
+
+# output $100 if it's other greeting
+
+main()
