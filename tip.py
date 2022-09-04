2173d3ea86dd9d180db4f6627e276dfd81c7173b
Jedrek Darowski <11andar07@gmail.com>
2022-09-04 13:29:11 +0200

    1. TODO list

---
+++ tip.py
@@ -0,0 +1,15 @@
+def main():
+
+    # Get input from user (meal prize and tip percentage)
+    # Calculate tip
+    # Output tip value
+
+def dollars_to_float(d):
+
+    # remove '$' and return amout as a float
+
+def percent_to_float(p):
+
+    # remove "%" and return as a float
+
+main()
