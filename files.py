2818666d2815ff95e68c11b75cfe701c4f65c8a3
Jedrek Darowski <11andar07@gmail.com>
2022-09-04 18:43:51 +0200

    4. Recognize files and output error if needed

--- files.py
+++ files.py
@@ -7,12 +7,25 @@ def main():
 def extension(f):
 
 #create list of available file extensions
-    file_extentions = ["GIF/.gif", "IMAGE/.jpeg", "PNG/.png", "PDF/.pdf", "TXT/.txt", "ZIP/.zip"]
+    file_extensions = ["GIF/.gif", "IMAGE/.jpeg", "PNG/.png", "PDF/.pdf", "TXT/.txt", "ZIP/.zip"]
     error = "application/octet-stream" 
 
-#recognize file by ".extension"
-
-#else output application/octet-stream
-
+#recognize file by ".extension", else output application/octet-stream
+    if ".gif" in f:
+        print(file_extensions[0])
+    elif ".jpeg" in f:
+        print(file_extensions[1])
+    elif ".jpg" in f:
+        print(file_extensions[1])
+    elif ".png" in f:
+        print(file_extensions[2])
+    elif ".pdf" in f:
+        print(file_extensions[3])
+    elif ".txt" in f:
+        print(file_extensions[4])
+    elif ".zip" in f:
+        print(file_extensions[5])    
+    else:
+        print(error)
 
 main()
