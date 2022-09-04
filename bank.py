1518ded89cf05a081f7a3521f55cc988a1ed96b8
Jedrek Darowski <11andar07@gmail.com>
2022-09-04 15:41:22 +0200

    4. Output /usr/bin/bash if greeting contains hello

--- bank.py
+++ bank.py
@@ -8,6 +8,8 @@ def check(g):
     pay_list = ["$0", "$20", "$100"]
 
 # output $0 if the greeting contains "hello"
+    if "hello" in g:
+        print(pay_list[0])
 
 # output $20 if the greeting startswith "h" but not "hello"
 
