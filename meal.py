ca9b1403e1684c20cca410e2dcff72c1faf2db2b
Jedrek Darowski <11andar07@gmail.com>
2022-09-05 17:45:02 +0200

    1. TODO list

---
+++ meal.py
@@ -0,0 +1,18 @@
+def main():
+    # prompt user for time
+    
+    # assign values to hours and minutes
+
+    # call convert()
+
+
+def convert(time):
+    # create list of meals
+    
+    # convert hours and minutes to float
+    
+    # determine when it's time for each meal
+
+
+if __name__ == "__main__":
+    main()
