dcc003b5fc383414133175c5ed2f377b87395c08
Jedrek Darowski <11andar07@gmail.com>
2022-09-07 01:22:18 +0200

    4. Moved time.split(:) to convert() function, converted hours and minutes to float, returned time as a float

--- meal.py
+++ meal.py
@@ -2,8 +2,8 @@ def main():
     # prompt user for time
     time = input("What's the time? ")
     
-    # assign values to hours and minutes
-    hours, minutes = time.split(":")
+    
+    
 
 
     # call convert()
@@ -13,12 +13,18 @@ def main():
 def convert(time):
     # create list of meal times
     meal_time = ["breakfast time", "lunch time", "dinner time"]
+
+    # assign values to hours and minutes
+    hours, minutes = time.split(":")
     
     # convert hours and minutes to float
-    
+    new_hours = float(hours)
+    new_minutes = float(minutes) / 60
+
+    print(new_hours + new_minutes)
     
     # determine when it's time for each meal
-
+    
 
 if __name__ == "__main__":
     main()
