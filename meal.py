6e88745f65fc731218a8937698fdca0179c24fb2
Jedrek Darowski <11andar07@gmail.com>
2022-09-07 01:13:01 +0200

    3. Created a list of meal times

--- meal.py
+++ meal.py
@@ -11,9 +11,11 @@ def main():
 
 
 def convert(time):
-    # create list of meals
+    # create list of meal times
+    meal_time = ["breakfast time", "lunch time", "dinner time"]
     
     # convert hours and minutes to float
+    
     
     # determine when it's time for each meal
 
