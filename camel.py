dc1ae15b0ec43c1db655c0a6c063791912a32294
Jedrek Darowski <11andar07@gmail.com>
2022-09-07 16:16:21 +0200

    1. TODO list

---
+++ camel.py
@@ -0,0 +1,15 @@
+def main():
+    # Get name of a variable in camelCase from user
+
+    # Call camel_to_snake()
+ 
+def camel_to_snake():
+    # Iterate over each character of user's input to find uppercase letters
+
+    # Put "_" in every spot where lowercase meets uppercase in that particular order    lowercase_uppercase
+
+    # Print snake_case
+
+main()
+
+     
