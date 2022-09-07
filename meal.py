f4be5d83dc0696b95f685506fcb1d81150ba267f
Jedrek Darowski <11andar07@gmail.com>
2022-09-07 02:01:54 +0200

    5. Determined when it's time for each meal

--- meal.py
+++ meal.py
@@ -17,14 +17,24 @@ def convert(time):
     # assign values to hours and minutes
     hours, minutes = time.split(":")
     
+    
     # convert hours and minutes to float
     new_hours = float(hours)
     new_minutes = float(minutes) / 60
 
-    print(new_hours + new_minutes)
+    time = new_hours + new_minutes
     
     # determine when it's time for each meal
     
+    if  7 <= time <= 8:
+        print(meal_time[0])
+    elif 12 <= time <= 13:
+        print(meal_time[1])
+    elif 18 <= time <= 19:
+        print(meal_time[2])
+    else:
+        print("It's not the time for a meal")
+
 
 if __name__ == "__main__":
     main()
