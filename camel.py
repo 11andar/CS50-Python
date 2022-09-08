1fbbe596791ce5878f8d5465d7c0445f41cb89a4
Jedrek Darowski <11andar07@gmail.com>
2022-09-08 16:02:30 +0200

    4. Program can now find uppercase letters

--- camel.py
+++ camel.py
@@ -5,9 +5,12 @@ def main():
     # Call camel_to_snake()
     camel_to_snake(camel_case)
  
-def camel_to_snake():
+def camel_to_snake(c):
     # Iterate over each character of user's input to find uppercase letters
-
+    for letter in c:
+        if letter.isupper() is True:
+            print(letter)
+    
     # Put "_" in every spot where lowercase meets uppercase in that particular order    lowercase_uppercase
 
     # Print snake_case
