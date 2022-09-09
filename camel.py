3dc854eb3b499bb7b1f6aa90644df672529578b1
Jedrek Darowski <11andar07@gmail.com>
2022-09-09 12:14:43 +0200

    6. Printed '_' before every uppercase letter and moved 'camel_to_snake' outside print() in main()

--- camel.py
+++ camel.py
@@ -2,20 +2,26 @@ def main():
     # Get name of a variable in camelCase from user
     camel_case = input("camelCase: ")
 
-    # Print snake_case and call camel_to_snake()
-    print(f"snake_case: {camel_to_snake(camel_case)}")
+    # Print snake_case 
+    print("snake_case: ", end = "")
+
+    # Call camel_to_snake()
+    camel_to_snake(camel_case)
  
 def camel_to_snake(c):
     # Iterate over each character of user's input to find uppercase letters
     for letter in c:
-        if letter.isupper() is True:
-            return True
+        if letter.isupper():
             
-        
-    
-    # Put "_" in every spot where lowercase meets uppercase in that particular order: lowercase_uppercase
+            # Loop goes through each letter and prints it unchanged if it's in lowercase 
+            # but if the letter is in uppercase it prints "_" before the letter and converts it to lowercase
+            print(f"_{letter.lower()}", end = "")
 
+        else:
+            print(letter, end = "")
     
+    print()
+
 
 main()
 
