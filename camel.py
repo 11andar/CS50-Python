a8083fb6070415d1fb4bb7e71d423a2160f13c26
Jedrek Darowski <11andar07@gmail.com>
2022-09-09 10:55:55 +0200

    5. Printed 'snake_case: ' in main()

--- camel.py
+++ camel.py
@@ -2,18 +2,20 @@ def main():
     # Get name of a variable in camelCase from user
     camel_case = input("camelCase: ")
 
-    # Call camel_to_snake()
-    camel_to_snake(camel_case)
+    # Print snake_case and call camel_to_snake()
+    print(f"snake_case: {camel_to_snake(camel_case)}")
  
 def camel_to_snake(c):
     # Iterate over each character of user's input to find uppercase letters
     for letter in c:
         if letter.isupper() is True:
-            print(letter)
+            return True
+            
+        
     
-    # Put "_" in every spot where lowercase meets uppercase in that particular order    lowercase_uppercase
+    # Put "_" in every spot where lowercase meets uppercase in that particular order: lowercase_uppercase
 
-    # Print snake_case
+    
 
 main()
 
