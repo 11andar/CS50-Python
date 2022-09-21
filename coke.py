661768a4fcaeda5c9bdb8bf22c797fd1e0e603e2
Jedrek Darowski <11andar07@gmail.com>
2022-09-21 11:52:18 +0200

    1. TODO list

---
+++ coke.py
@@ -0,0 +1,7 @@
+# Print 'Amount Due = 50' and prompt user for input 'Insert Coin'
+
+# Each time user inserts a coin inform him about amount due and prompt for a coin again
+
+# User can insert only 25, 10 or 5 cents
+
+# Print 'Change Owed' at the end
