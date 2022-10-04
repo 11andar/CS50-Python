91f2a3cd0d4282d9a00388ac644b0fefb4b4e253
Jedrek <92921076+11andar@users.noreply.github.com>
2022-10-04 10:09:06 +0200

    Delete coke.py

--- coke.py
+++
@@ -1,7 +0,0 @@
-# Print 'Amount Due = 50' and prompt user for input 'Insert Coin'
-
-# Each time user inserts a coin inform him about amount due and prompt for a coin again
-
-# User can insert only 25, 10 or 5 cents
-
-# Print 'Change Owed' at the end
