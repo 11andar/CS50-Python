73593727c386a72b7e043a2782ae29ba5167ae34
Jedrek Darowski <11andar07@gmail.com>
2022-10-04 10:20:05 +0200

    3. Informing about AD left

--- coke.py
+++ coke.py
@@ -1,15 +1,15 @@
 # Print ammount due and prompt user for inserting a coin
+# Each time user inserts a coin inform him about ammount due left and prompt for a coin again
 
 prize = 50
 
 while prize > 0:
     print(f"Ammount Due: {prize}")
 
-    insert = int(input("Insert Coin: "))
- 
-
 # User can only insert 25, 10 or 5 cents
+    insert = int(input("Insert Coin [25, 10, 5]: "))
 
-# Each time user inserts a coin inform him about ammount due left and prompt for a coin again
+    prize = prize - insert
+ 
 
 # Print change owed at the end
