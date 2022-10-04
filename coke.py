f79e1391a20a267bb95c7d519833e9389ed6824e
Jedrek Darowski <11andar07@gmail.com>
2022-10-04 10:16:19 +0200

    2. Printed AD and prompted for insert

--- coke.py
+++ coke.py
@@ -1,5 +1,13 @@
 # Print ammount due and prompt user for inserting a coin
 
+prize = 50
+
+while prize > 0:
+    print(f"Ammount Due: {prize}")
+
+    insert = int(input("Insert Coin: "))
+ 
+
 # User can only insert 25, 10 or 5 cents
 
 # Each time user inserts a coin inform him about ammount due left and prompt for a coin again
