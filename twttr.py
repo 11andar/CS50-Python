52f6b229c69dc2069523229b6b996b5a425f910c
Jedrek Darowski <11andar07@gmail.com>
2022-10-06 14:29:29 +0200

    2. Input from user

--- twttr.py
+++ twttr.py
@@ -1,9 +1,13 @@
 # Define main() function
+def main():
     
     # Take input from user
+    word = input("Input: ")
 
 # Define short() function
 
     # Iterate over each character of input to find vowels
 
     # Print user's word without vowels
+
+main()
