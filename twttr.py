41cdf3e318ef19424116ee1f3dd63642ba498e94
Jedrek Darowski <11andar07@gmail.com>
2022-10-06 08:57:20 +0200

    1. TODO list

---
+++ twttr.py
@@ -0,0 +1,9 @@
+# Define main() function
+    
+    # Take input from user
+
+# Define short() function
+
+    # Iterate over each character of input to find vowels
+
+    # Print user's word without vowels
