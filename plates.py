c6c716a4870aa2eae98f90f5da1c4e66fbe6ecc7
Jedrek Darowski <11andar07@gmail.com>
2022-10-10 12:25:06 +0200

    1. TODO list

---
+++ plates.py
@@ -0,0 +1,19 @@
+# Define main()
+
+    # Take input from user 
+
+    # If plate is valid print 'Valid'
+
+    # Else print 'Invalid'
+
+# Define is_valid()
+
+    # Must start with at least two letters
+
+    # Minimum 2 and maximum 6 characters
+
+    # Numbers at the end
+
+    # First number cannot be 0
+
+    # No special signs
