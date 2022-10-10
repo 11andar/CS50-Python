4a47755a97f807cd0e75ed4c0a79ede9cd707ff3
Jedrek Darowski <11andar07@gmail.com>
2022-10-10 12:41:26 +0200

    4. Must start with at least two letters

--- plates.py
+++ plates.py
@@ -2,21 +2,31 @@
 def main():
     # Take input from user 
     plate = input("Plate: ")
+    plate = plate.upper()
+    
     # If plate is valid print 'Valid'
     if is_valid(plate) is True:
         print("Valid")
+    
     # Else print 'Invalid'
     else:
         print("Invalid")
-        
+
 # Define is_valid()
+def is_valid(p):
+    for character in p:
+        # Must start with at least two letters
+        if p[0:2].isalpha():
+            return True
+        
+        # Minimum 2 and maximum 6 characters
+    
+        
+        # Numbers at the end
 
-    # Must start with at least two letters
+        
+        # First number cannot be 0
 
-    # Minimum 2 and maximum 6 characters
-
-    # Numbers at the end
-
-    # First number cannot be 0
-
-    # No special signs
+        
+        # No special signs
+main()
