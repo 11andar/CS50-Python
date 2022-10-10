6ed3ad2a6c7d92b00ae92ead59bfe1d2270c5d66
Jedrek Darowski <11andar07@gmail.com>
2022-10-10 11:08:15 +0200

    4. Iterating over each character

--- twttr.py
+++ twttr.py
@@ -11,7 +11,8 @@ def short(w):
     #Create list of vowels
     vowel = ["a", "e", "u", "i", "o"]
     # Iterate over each character of input to find vowels
-
+    for letter in w:
+        
     # Print user's word without vowels
 
 main()
