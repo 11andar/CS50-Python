a355ec96e31ca921dda73a013e3dc10030575886
Jedrek Darowski <11andar07@gmail.com>
2022-10-14 21:18:18 +0200

    3. Dictionary 'fruit : calories'

--- nutrition.py
+++ nutrition.py
@@ -2,6 +2,28 @@ def main():
     # Ask user for fruit
     choice = input("Fruit: ")
     # Create a dictionary which assigns calories to fruits
+    fruits = {
+        "Apple" : 130
+        "Avocado" : 50
+        "Banana" : 110
+        "Cantaloupe" : 50
+        "Grapefruit" : 60
+        "Grapes" : 90
+        "Honeydew Melon" : 50
+        "Kiwifruit" : 90
+        "Lemon" : 15
+        "Lime" : 20
+        "Nectarine" : 60
+        "Orange" : 80
+        "Peach" : 60
+        "Pear" : 100
+        "Pineapple" : 50
+        "Plums" : 70
+        "Strawberries" : 50
+        "Sweet Cherries" : 100
+        "Tangerine" : 50
+        "Watermelon" : 80
+    }
 
     # Create a loop that prints amount of calories of fruit which user chose
 
