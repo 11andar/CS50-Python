c717b7e953be24a09854bdbb7fbc94b5d8185201
Jedrek Darowski <11andar07@gmail.com>
2022-10-14 21:08:35 +0200

    2. Ask user for input

--- nutrition.py
+++ nutrition.py
@@ -1,6 +1,6 @@
 def main():
     # Ask user for fruit
-
+    choice = input("Fruit: ")
     # Create a dictionary which assigns calories to fruits
 
     # Create a loop that prints amount of calories of fruit which user chose
