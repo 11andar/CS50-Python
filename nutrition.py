9a603f996492f0947af2a0f4ae712c19b190546c
Jedrek Darowski <11andar07@gmail.com>
2022-10-14 21:06:03 +0200

    1. TODO list

---
+++ nutrition.py
@@ -0,0 +1,8 @@
+def main():
+    # Ask user for fruit
+
+    # Create a dictionary which assigns calories to fruits
+
+    # Create a loop that prints amount of calories of fruit which user chose
+
+main() 
