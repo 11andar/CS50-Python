aece20c437d24d49cb3b500f553adce422d96223
Jedrek Darowski <11andar07@gmail.com>
2022-10-15 20:50:45 +0200

    1. TODO list

---
+++ fuel.py
@@ -0,0 +1,10 @@
+def main():
+# Prompt user for fraction formatted as X/Y
+
+# If there is only 1% or less left output E
+
+# If there is 99% or more output F
+
+# Ignore ValueError and ZeroDivisionError
+
+main()
