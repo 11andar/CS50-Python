fa7b59dbbad0423d39ce6d601376c71f63125cc0
Jedrek Darowski <11andar07@gmail.com>
2022-10-21 22:09:51 +0200

    6. Running the program until input is correct

--- fuel.py
+++ fuel.py
@@ -1,31 +1,36 @@
 def main():
     
-    try:
-    # Prompt user for fraction formatted as X/Y
-        fraction = input("Fraction: ")
-    
-    # Assign first value to x and second value to y    
-        x, y = fraction.split("/")
+    # Keep running the program until user inputs correct values
+    while True:     
+        try:
+        # Prompt user for fraction formatted as X/Y
+            fraction = input("Fraction: ")
+            
+        # Assign first value to x and second value to y    
+            x, y = fraction.split("/")
 
-    # Convert values to ints
-        x = int(x)
-        y = int(y)
+        # Convert values to ints
+            x = int(x)
+            y = int(y)
+            
+            divide = int((x/y) * 100)
+
+        # If there is only 1% or less left output E
+            if divide <= 1:
+                print("E")
+        # If there is 99% or more output F
+            elif divide >= 99:
+                print("F")
+        # Else output how full is the tank (in %)
+            else:
+                print(f"Your tank is {divide}% full")
         
-        divide = int((x/y) * 100)
-
-    # If there is only 1% or less left output E
-        if divide <= 1:
-            print("E")
-    # If there is 99% or more output F
-        elif divide >= 99:
-            print("F")
-    # Else output how full is the tank (in %)
-        else:
-            print(f"Your tank is {divide}% full")
-    
-    # Ignore ValueError and ZeroDivisionError
-    except (ValueError, ZeroDivisionError):
-        pass
+        # Break out of the loop if values are correct
+            break
+        
+        # Ignore ValueError and ZeroDivisionError
+        except (ValueError, ZeroDivisionError):
+            pass
 
 
 main()
