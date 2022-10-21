1a7f07957a28c75391c4480a4391089952602447
Jedrek Darowski <11andar07@gmail.com>
2022-10-21 22:02:03 +0200

    4. Fuel tank status

--- fuel.py
+++ fuel.py
@@ -8,15 +8,20 @@ def main():
         x, y = fraction.split("/")
 
     # Convert values to floats
-        x = float(x)
-        y = float(y)
+        x = int(x)
+        y = int(y)
+        
+        divide = int((x/y) * 100)
 
     # If there is only 1% or less left output E
-        
+        if divide <= 1:
+            print("E")
     # If there is 99% or more output F
-
+        elif divide >= 99:
+            print("F")
     # Else output how full is the tank (in %)
-
+        else:
+            print(f"Your tank is {divide}% full")
     # Ignore ValueError and ZeroDivisionError
     
 
