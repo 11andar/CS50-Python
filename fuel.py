d44570cd39cd1aeb622977a2a8bbed110eadbe78
Jedrek Darowski <11andar07@gmail.com>
2022-10-21 21:48:52 +0200

    3. Convert to floats

--- fuel.py
+++ fuel.py
@@ -1,14 +1,18 @@
 def main():
     
-    try:
+    
     # Prompt user for fraction formatted as X/Y
         fraction = input("Fraction: ")
     
     # Assign first value to x and second value to y    
         x, y = fraction.split("/")
 
+    # Convert values to floats
+        x = float(x)
+        y = float(y)
+
     # If there is only 1% or less left output E
-    
+        
     # If there is 99% or more output F
 
     # Else output how full is the tank (in %)
