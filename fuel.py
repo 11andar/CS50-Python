eb9b50287723a43deb99f3fece135252c00b8e6d
Jedrek Darowski <11andar07@gmail.com>
2022-10-21 21:45:35 +0200

    2. Get input, assign values

--- fuel.py
+++ fuel.py
@@ -1,13 +1,19 @@
 def main():
+    
+    try:
     # Prompt user for fraction formatted as X/Y
+        fraction = input("Fraction: ")
+    
+    # Assign first value to x and second value to y    
+        x, y = fraction.split("/")
 
     # If there is only 1% or less left output E
-
+    
     # If there is 99% or more output F
 
     # Else output how full is the tank (in %)
 
     # Ignore ValueError and ZeroDivisionError
-
+    
 
 main()
